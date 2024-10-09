4c2a13e1bc4386ab3758c62255a2db6fcb602170
yy214123 <yy214123@gmail.com>
2024-10-09 17:18:36 +0800

    Remove print_fabs_Result function
    The print_fabs_Result function is no longer needed and has been
    removed to simplify the codebase.

--- Assignment1.s
+++ Assignment1.s
@@ -6,38 +6,12 @@ str2:     .string " is "
 
 .text
 main:
-        lw a0, float_value
         jal ra, fabsf
-        
-        jal ra, print_fabs_Result
-
         li a7, 10               
         ecall                  
-fabsf:        
+fabsf:
+        lw a0, float_value        
         lw a1, abs_mask      
         and a1, a0, a1      
         ret      
         
-print_fabs_Result:
-        mv t0, a0
-        mv t1, a1  
-        
-        # print "The absolute value of"
-        la a0, str1
-        li a7, 4  
-        ecall
-        
-        mv a0, t0                  
-        li a7, 1                
-        ecall 
-        
-        # print "is"
-        la a0, str2
-        li a7, 4  
-        ecall
-        
-        mv a0, t1                 
-        li a7, 1                
-        ecall 
-        
-        ret 
